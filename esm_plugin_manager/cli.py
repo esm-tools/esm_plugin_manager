f7652bb4b500f94dbc43fe13a1b041aa357c54f2
Sebastian Wahl <swahl@geomar.de>
2020-08-31 13:41:00 +0200

    added extra print statement

--- esm_plugin_manager/cli.py
+++ esm_plugin_manager/cli.py
@@ -2,9 +2,10 @@ from .esm_plugin_manager import find_installed_plugins
 
 def main():
     discovered_plugins = find_installed_plugins()
+    print("The following plugins are installed and available:")
     for plugin_name in discovered_plugins:
         plugin_code = discovered_plugins[plugin_name]["callable"]
-        print(plugin_name)
+        print(" - "+plugin_name)
         doc = getattr(plugin_code, "__doc__")
         if doc:
             print("-"*len(plugin_name))
